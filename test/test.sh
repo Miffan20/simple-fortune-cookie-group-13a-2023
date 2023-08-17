03210de24c77d82d4d329ad502fd566b81d0c805
Marcel Staal <niels21@student.sdu.dk>
2023-08-17 12:45:47 +0000

    Update test & create test step in workflow

--- test/test.sh
+++ test/test.sh
@@ -2,6 +2,8 @@ RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/)
 
 if [ "$RESPONSE" -eq 200 ]; then
   echo "Test passed: Site on localhost:8080 is up and accessible."
+  exit 0
 else
   echo "Test failed: Site on localhost:8080 returned $RESPONSE"
+  exit 1
 fi
