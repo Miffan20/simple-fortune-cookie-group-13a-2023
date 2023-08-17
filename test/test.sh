57e1b40ee417bd417a0f027ab30400ec8db2859d
Marcel Staal <niels21@student.sdu.dk>
2023-08-17 15:38:04 +0000

    Revert "Update test url"
    This reverts commit 4931c3a367777c28e606cef75f5c10f18a4f77a6.

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,4 @@
-RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/)
+RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" http://workstation-13.sdu1.eficode.academy:8080/)
 
 if [ "$RESPONSE" -eq 200 ]; then
   echo "Test passed: Site is up and accessible."
