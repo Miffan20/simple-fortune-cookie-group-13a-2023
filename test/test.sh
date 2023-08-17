1221ba1e2d280bdabbd710e0d0ecb65c2fba4841
Marcel Staal <niels21@student.sdu.dk>
2023-08-17 13:10:39 +0000

    Update test return string

--- test/test.sh
+++ test/test.sh
@@ -1,9 +1,9 @@
 RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" http://workstation-13.sdu1.eficode.academy:8080/)
 
 if [ "$RESPONSE" -eq 200 ]; then
-  echo "Test passed: Site on localhost:8080 is up and accessible."
+  echo "Test passed: Site is up and accessible."
   exit 0
 else
-  echo "Test failed: Site on localhost:8080 returned $RESPONSE"
+  echo "Test failed: Site returned $RESPONSE"
   exit 1
 fi
